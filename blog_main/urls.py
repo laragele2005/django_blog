0fd2635a6431be1f6621b6c6899ca6f525cc16e7
laragele2005 <larugele@gmail.com>
2026-05-03 18:45:55 -0300

    posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,4 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home,name='home'),
+    path('category/', include('blogs.urls'))
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Blog, Category
 # Create your views here.
+
+
+def post_by_category(request, category_id):
+    # Fetch the posts that belongs to the category id
+    posts = Blog.objects.filter(category_id=category_id, status="Published")
+    # Try except blog when the category number does not exist, it will redirect to home page
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except: 
+    #     return redirect('home')
+    
+    category= get_object_or_404(Category,pk=category_id)
+    
+    context = {
+        'posts':posts,
+        'category':category
+    }
+    return render(request, 'posts_by_category.html', context)
