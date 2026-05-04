49665eb888aab8739a5a6a507e720f78103e3ef2
laragele2005 <larugele@gmail.com>
2026-05-04 10:49:40 -0300

    Blogs and search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from blogs.models import Category 
 from blogs.models import Blog
+from assignments.models import About
 
 def home(request):
     categories = Category.objects.all()
     featured_posts= Blog.objects.filter(is_featured=True,  status="Published").order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status="Published")
-    
-    
-    
+    try:
+       about = About.objects.first()
+    except:
+       about = None
+
     context = {
         'categories': categories,
         'featured_posts': featured_posts,
         'posts':posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
